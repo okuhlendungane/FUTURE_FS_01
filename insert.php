cfdb5107108a6e48125f350d960da849ec4eecd7
Okuhle Ndungane <ndunganeokuhle66@gmail.com>
2026-04-06 13:54:04 +0200

    added Skills and Contact section

---
+++ insert.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Database Insert</title>
+</head>
+<body>
+    <?php
+    // Database connection
+    $conn = mysqli_connect("localhost", "root", "", "personal_website");
+    // Check connection
+    if ($conn === false) {
+        die("ERROR: Could not connect. " . mysqli_connect_error());
+    }
+    // Collect form data
+    $email = $_REQUEST['email'];
+    $message = $_REQUEST['message'];
+    // Insert data into the database
+    $sql = "INSERT INTO messages (email, message) VALUES ('$email','$message')";
+    if (mysqli_query($conn, $sql)) {
+        echo "<h3>Data stored in the database successfully.</h3>";
+        echo nl2br("\n$email\n $message");
+    } else {
+        echo "ERROR: Sorry $sql. " . mysqli_error($conn);
+    }
+    // Close connection
+    mysqli_close($conn);
+    ?>
+
+</body>
+</html>
